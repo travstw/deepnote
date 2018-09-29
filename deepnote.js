21aa0a9b41bde906f1fabf1e57637ad30b3c3e50
travstw <travstw@gmail.com>
2018-09-29 01:31:53 -0700

    set defaults

--- deepnote.js
+++ deepnote.js
@@ -7,23 +7,37 @@ let panGenerator;
 let oscillators = []; 
 
 //Parameters
-let voices = 30;
-let initialFreqLow = 200;
-let initialFreqHigh = 400;
-let final_freqs = [72, 144, 288, 576, 1152, 108, 216, 432, 864, 1458];
-let wave = 'sawtooth';
-
+let voices;
+let initialFreqLow;
+let initialFreqHigh;
+let final_freqs;
+let wave;
+let length;
+let rampStart;
+let rampLength;
+let fadeIn;
+let fadeOutStart;
 
 //DOM elements
 const playButtonElem = document.getElementById('playButton');
+const resetDefaultsElem = document.getElementById('resetDefaults');
 const initialFreqLowElem = document.getElementById('initialFreqLow');
 const initialFreqHighElem = document.getElementById('initialFreqHigh');
 const voicesElem = document.getElementById('voices');
-const waveElem= document.getElementById('wave');
+const waveElem = document.getElementById('wave');
+const lengthElem = document.getElementById('length');
+const rampStartElem = document.getElementById('rampStart');
+const rampLengthElem = document.getElementById('rampLength');
+const fadeInElem = document.getElementById('fadeIn');
+const fadeOutStartElem = document.getElementById('fadeOutStart');
 
 //Event Listeners
 playButtonElem.addEventListener('click', () => {
     play();
+});
+
+resetDefaultsElem.addEventListener('click', () => {
+    setDefaultParams();
 });
 
 initialFreqLowElem.addEventListener('change', () => {
@@ -40,10 +54,35 @@ voicesElem.addEventListener('change', () => {
 
 waveElem.addEventListener('change', () => {
     wave = waveElem.value;
-    console.log(wave);
-})
+});
+
+lengthElem.addEventListener('change', () => {
+    length = parseInt(lengthElem.value);
+   
+});
+
+rampStartElem.addEventListener('change', () => {
+    rampStart = parseInt(rampStartElem.value);
+    console.log(rampStart);
+});
+
+rampLengthElem.addEventListener('change', () => {
+    rampLength = parseInt(rampLengthElem.value);
+    console.log(rampLength);
+});
+
+fadeInElem.addEventListener('change', () => {
+    fadeIn = parseInt(fadeInElem.value);
+    console.log(fadeIn);
+});
+
+fadeOutStartElem.addEventListener('change', () => {
+    fadeOutStart = parseInt(fadeOutStartElem.value);
+    console.log(fadeOutStart);
+});
 
 
+setDefaultParams();
 
 
 function init() {
@@ -59,15 +98,15 @@ function createOscillators() {
 
     for (let i = 0; i < voices; i++) {
         const finishingNote = getFrequency();
-        const finishingPan = finishingNote < 300 ? 0.0 : panGenerator.next().value;
-        const finishingGain = (finishingNote > 864) ? (Math.random() * 0.4) + 0.2 : (Math.random() * 0.85) + 0.65 ;
+        const finishingPan = finishingNote < 300 ? 0.00001 : panGenerator.next().value;
+        const finishingGain = (finishingNote > 400) ? (Math.random() * 0.4) + 0.2 : (Math.random() * 0.85) + 0.65;
         const freq = Math.floor((Math.random() * initialFreqHigh) + initialFreqLow);
         let playing = false;
         
         const osc = new OscillatorNode(audioContext, {
             type: setWave(),
             frequency: freq,
-            detune: finishingNote === 1458 ? Math.floor((Math.random() * 10) + 1) : 0
+            detune: finishingNote === 1458 ? Math.floor((Math.random() * 20) + 1) : 0
         });
 
         const lp = new BiquadFilterNode(audioContext, {
@@ -82,7 +121,7 @@ function createOscillators() {
         })
 
         const panner = new PannerNode(audioContext, {
-            positionX: 0.0
+            positionX: 0.00
             // positionX: 0.001
         });
 
@@ -128,18 +167,22 @@ function play() {
     init();
 
     oscillators.forEach(osc => {
-        osc.gain.gain.exponentialRampToValueAtTime(osc.finishingGain, audioContext.currentTime + 12);
-        const fadeoutStart = audioContext.currentTime + 24;
-        osc.gain.gain.setTargetAtTime(0.0, fadeoutStart, 1.0);
+        osc.gain.gain.exponentialRampToValueAtTime(osc.finishingGain, audioContext.currentTime + fadeIn);
 
-        const pannerStart = audioContext.currentTime + 10;
-        const pannerTime = (Math.random() * 0.85) + 0.75;
-        osc.panner.positionX.setTargetAtTime(osc.finishingPan, pannerStart, pannerTime);
-
-        const pitchStart = audioContext.currentTime + 10;
-        const pitchTime = (Math.random() * 0.85) + 0.75;
-        osc.osc.frequency.setTargetAtTime(osc.finishingNote,  pitchStart, pitchTime);
-    
+        setTimeout(() => {
+            osc.gain.gain.exponentialRampToValueAtTime(0.0001, audioContext.currentTime + (length - fadeOutStart));
+        }, 1000 * fadeOutStart);
+        
+        setTimeout(() => {
+            const fadeInTime = rampLength + ((Math.random() * 1) + -0.5);
+            osc.panner.positionX.exponentialRampToValueAtTime(osc.finishingPan, audioContext.currentTime + fadeInTime);
+        }, 1000 * rampStart);
+        
+        setTimeout(() => {
+            const rampInTime = rampLength + ((Math.random() * 1) + -0.5);
+            osc.osc.frequency.exponentialRampToValueAtTime(osc.finishingNote, audioContext.currentTime + rampInTime);
+        }, 1000 * rampStart);
+        
         osc.osc.start();
         osc.playing = true;
         
@@ -184,6 +227,28 @@ function getPanGenerator() {
         }
        
     })();
+}
+
+function setDefaultParams() {
+    voices = 30;
+    voicesElem.value = 30;
+    initialFreqLow = 200;
+    initialFreqLowElem.value = 200;
+    initialFreqHigh = 400;
+    initialFreqHighElem.value = 400;
+    final_freqs = [72, 144, 288, 576, 1152, 108, 216, 432, 864, 1458];
+    wave = 'sawtooth';
+    waveElem.value = 'sawtooth';
+    length = 30;
+    lengthElem.value = 30;
+    rampStart = 13;
+    rampStartElem.value = 13;
+    rampLength = 5;
+    rampLengthElem.value = 5;
+    fadeIn = 14;
+    fadeInElem.value = 14;
+    fadeOutStart = 26;
+    fadeOutStartElem.value = 26;
 }
 
 function setWave() {
